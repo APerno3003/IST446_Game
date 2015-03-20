9b5375e0849413a92734a33036b93bf8d4b32cbe
APerno3003 <APerno3003@gmail.com>
2015-03-19 22:59:03 -0400

    Added a script for reloading a level
    Added prefab _LevelManager and attached Reload_Scene.cs to it. These two
    things together allows you to press the r key to reload the current
    level/scene

---
+++ Assets/Script/Reload_Scene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reload_Scene : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel (Application.loadedLevelName);
+			
+		}
+	}
+}
