f06949747c0df8f77af91e3734f4c4bcce136c87
PochGee <pochologayan@yahoo.com>
2015-03-22 00:04:58 -0400

    Fixed the jump
    Jumping does not alter rotation anymore, so falling off the platform no
    longer occurs. A smooth jump was achieved by letting update() translate
    player's y-position until max jump height is reached.
    Changes in Movement script (labeled 1-4 in code comments):
    1.) Reset object rotation every frame to avoid falling off platform
    2.) Initial small jump just to get player off the ground
    3.) Once off the ground, IF-test in update() keeps incrementing height
    variable and translating player upwards until maximum height is reached
    4.) Upon hitting the ground from gravity, height is reset

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 	public float moveSpeed = 6.0F;
-	public float jumpSpeed = 8.0F;
+	public float jumpSpeed = 2.0F;
 	public float gravity = 20.0F;
-	public bool onGround = false;
+	public bool onGround = true; // start off true so no auto jump
+
+	public float jump_height = 0.0F;
+	public const float MAX_JUMP_HEIGHT = 25.0F;
+	public const float JUMP_DELTA = 1.0F;
+	public const float SMALL_JUMP = 2.0F;
+	public const float JUMP_SPEED = 8.0F;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -16,6 +23,11 @@ public class Movement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		// 1.)
+		// reset rotation every frame, so object's rotation is not affected by jump
+		// fixes falling off platform bug
+		transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+
 		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate (new Vector3 (-moveSpeed, 0, 0) * Time.deltaTime);
 		}
@@ -26,22 +38,33 @@ public class Movement : MonoBehaviour
 
 
 		if (onGround && (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow))) {
-			//transform.Translate (new Vector3 (0, moveSpeed, 0) * Time.deltaTime);
-			transform.Translate (Vector3.up * jumpSpeed* Time.deltaTime, Space.World);
-			//transform.Translate (Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
-			//transform.Translate(Vector2.up * 100 * Time.deltaTime, Space.World);
+
+			// non-smooth jump
+			//transform.Translate (new Vector3 (0, jumpSpeed * 15, 0) * Time.deltaTime, Space.World);
+
+			// 2.)
+			// smooth jump 
+			// jumps enough to get object off ground(set onGround to false), and update loop() will do the rest of the jump
+			transform.Translate (new Vector3 (0, SMALL_JUMP, 0) * Time.deltaTime, Space.World);
 		}
-		/*if (Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.DownArrow)) {
-						transform.Translate (new Vector3 (0, -moveSpeed, 0) * Time.deltaTime);
-			
-		}*/
 
+		// 3.)
+		// Object is in the middle of jumping, keep going up until max height is reached
+		if (!onGround && jump_height < MAX_JUMP_HEIGHT)
+		{
+			transform.Translate(new Vector3 (0, JUMP_SPEED, 0) * Time.deltaTime, Space.World);
+			jump_height += JUMP_DELTA;
+		}
+
+		//print (jump_height);
 	}
-
+	
 	void  OnCollisionEnter (Collision theCollision)
 	{
 		if (theCollision.gameObject.name == "Floor") {
-			onGround = true;
+			// 4.) upon hitting ground, reset jump height for next jump
+			onGround = true; 
+			jump_height = 0.0F;
 		}
 	}
 
