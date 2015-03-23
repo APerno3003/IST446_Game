c29844e5ff011fe42ffe8157f1aa4c1d9a150bff
APerno3003 <APerno3003@gmail.com>
2015-03-23 19:31:53 -0400

    Added newEnemy Type
    Added a new Enemy Type, new stage, wall prefab, nonMoving Enemy prefab,
    and Moving Enemy prefab
    
    The Enemy has the ability to move left and right based on a few factors.
    The speed determines how fast they travel the distance. The distance is
    how far from the original position you want the Enemy to move. The
    Original Position is were the Enemy starts. There also a bool that
    states which direction it should go first left or right.

---
+++ Assets/Script/Bad_Guy_Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bad_Guy_Movement : MonoBehaviour {
+
+	private Vector3 velocity;
+	private Transform _transform;
+	public float distance;
+	public float speed;
+	public Vector3 _originalPosition;
+	public bool isGoingLeft;
+
+	void Start () {
+		_originalPosition = gameObject.transform.position;
+		_transform = GetComponent<Transform>();
+		velocity = new Vector3(speed,0,0);
+		_transform.Translate ( velocity.x*Time.deltaTime,0,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//keeps badguy on the 0 z plane
+		transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+		transform.position = (new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, 0));
+
+		float distFromStart = transform.position.x - _originalPosition.x;   
+
+		if (isGoingLeft)
+		{ 
+			// If gone too far, switch direction
+			if (distFromStart < -distance)
+			{
+				isGoingLeft = false;
+			}
+			_transform.Translate (-velocity.x * Time.deltaTime, 0, 0);
+		}
+		else
+		{
+			// If gone too far, switch direction
+			if (distFromStart > distance)
+			{
+				isGoingLeft = true;
+			}
+			
+			_transform.Translate (velocity.x * Time.deltaTime, 0, 0);
+		}
+	}
+
+}
---
+++ Assets/Script/Bullet_Destory_Self.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Destory_Self : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void  OnCollisionEnter ( Collision theCollision  ){
+		if (theCollision.gameObject.name == "Bad_Guy" || theCollision.gameObject.name == "Door" 
+		    || theCollision.gameObject.name == "Wall"  || theCollision.gameObject.name == "Bullet" 
+		    || theCollision.gameObject.name == "Floor") {
+			
+			Destroy (gameObject);
+		} 
+		
+		else if (theCollision.gameObject.name == "Player" || theCollision.gameObject.name == "Diamond(Clone)" || theCollision.gameObject.name == "Diamond") {
+			
+			Destroy(theCollision.gameObject);
+			Destroy(gameObject);
+		}
+
+	}
+
+}
